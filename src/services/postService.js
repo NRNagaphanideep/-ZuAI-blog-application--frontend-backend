7a96d7d6a64de350554e6b986f5dce79a009e64d
Nagaphanideep <95171605+NRNagaphanideep@users.noreply.github.com>
2024-08-26 04:54:34 +0530

    Update postService.js

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,7 +1,7 @@
 // src/services/postService.js
 import axios from "axios";
 
-const API_URL = "https://phaniblogapplication.netlify.app/";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/posts";
 
 export const getPosts = async () => {
   try {
